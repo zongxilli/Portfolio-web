d2de5b87838228e54e3da0d3703f8f3c792206e4
zongxi li <zongxi2014@gmail.com>
2021-06-04 11:21:13 -0400

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
 
 function App() {
-	return <h2>App</h2>;
+	return (
+		<Navbar />
+	);
 }
 
 export default App;
