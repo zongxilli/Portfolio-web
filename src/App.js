971fa7f7ebf97ed95f430bba37f1907d8d4f57fe
zongxi li <zongxi2014@gmail.com>
2021-06-04 15:08:11 -0400

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar'
+import Dynamic from './components/Dynamic';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
 	return (
-		<Navbar />
+		<>
+			<Dynamic />
+			<Navbar />
+			<Header />
+		</>
 	);
 }
 
