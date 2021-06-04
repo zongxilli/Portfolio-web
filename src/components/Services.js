a0409cdb0cf080725b075ad8107e087129c92355
zongxi li <zongxi2014@gmail.com>
2021-06-04 17:29:13 -0400

    Services Completed

---
+++ src/components/Services.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import {faDesktop, faFileCode} from "@fortawesome/free-solid-svg-icons";
+
+const Services = () => {
+	return (
+		<div className="services">
+			<h1 className="py-5">My Services</h1>
+				<div className="container">
+					<div className="row">
+						{/******************************/}
+						<div className="col-md-3 col-sm-6">
+							<div className="box">
+								<div className="circle">
+									<FontAwesomeIcon className="icon" icon={faDesktop} size="2x" />
+								</div>
+								<h3>Web Design</h3>
+								<p>I enjoy designing beautiful webs</p>
+							</div>
+						</div>
+						{/******************************/}
+						<div className="col-md-3 col-sm-6">
+							<div className="box">
+								<div className="circle">
+									<FontAwesomeIcon className="icon" icon={faFileCode} size="2x" />
+								</div>
+								<h3>Web Development</h3>
+								<p>I also enjoy building webs especially for gorgeous webs</p>
+							</div>
+						</div>
+						{/******************************/}
+						<div className="col-md-3 col-sm-6">
+							<div className="box">
+								<div className="circle">
+									<FontAwesomeIcon className="icon" icon={faFacebookF} size="2x" />
+								</div>
+								<h3>App Design</h3>
+								<p>I design Android & IOS Apps</p>
+							</div>
+						</div>
+						{/******************************/}
+						<div className="col-md-3 col-sm-6">
+							<div className="box">
+								<div className="circle">
+									<FontAwesomeIcon className="icon" icon={faGoogle} size="2x" />
+								</div>
+								<h3>App development</h3>
+								<p>I also build Android & IOS Apps</p>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+	);
+};
+
+export default Services;
