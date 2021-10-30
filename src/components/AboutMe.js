9c3e61fac660e1f5fab6b47e2cde2572b5598f48
zongxilli <zongxi2014@gmail.com>
2021-10-29 23:38:16 -0400

    make some changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,35 +13,22 @@ const AboutMe = () => {
 				<div className='col-lg-6 col-xm-12'>
 					<h1 className='about-heading'>About Me</h1>
 					<p>
-						Hi! I am Zongxi(Kenny), I am a new grad in 2021 with a
-						Bachelor's degree in Mathematics & Statistics from the
-						University of Toronto Scarborough.
-					</p>
-					<p>
-						I am good at developing full-stack React applications
-						especially with Redux, Node.js, Express, MongoDB,
-						PostgreSQL and I have experience in AWS like S3,
-						Cognito, also google APIs & firebase ...
-					</p>
-					<p>
-						What I did in my past internship: <br />
-						- Designed and implemented a powerful image detection &
-						annotation React application with React, Redux, Express,
-						Node.js, Bootstrap, Sequelize, Mongoose ... It is able
-						to draw, move and delete many shapes with the
-						auto-complete and zoom in/out feature, also it is able
-						to store the relative coordinates data interacting with
-						2 backend servers of 2 databases (PostgreSQL & MongoDB)
-						<br />- Created responsive UI with Semantic UI and
-						Bootstrap <br />
-						- Achieved MongoDB and PostgreSQL databases connection
-						and migration with Mongoose, Knex and Sequelize
-						<br /> - Managed user authentication and authorization
-						with AWS Cognito. Integrated AWS S3 into the application
-						for storing and retrieving data
-						<br /> - Created RESTful APIs with Node.js & Express and
-						use Axios & Cors to make synchronize HTTP requests and
-						promise to APIs
+						<br />
+						Hi! I am Zongxi(Kenny), I graduated from the University
+						of Toronto Scarborough with an Honours Bachelor of
+						Science degree in Mathematics & Statistics in 2021
+						<br />
+						<br />
+						The interesting thing is that I found I love
+						programming much more than mathematics and statistics in
+						the last year of my study at UofT...
+						<br />
+						<br />
+						But fortunately, I am a full stack developer at Mosaic
+						Manufacturing now
+						<br />
+						<br />
+						Nobody knew that in the future what matter will have :)
 					</p>
 				</div>
 			</div>
